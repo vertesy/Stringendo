2858535ae3e301ee0eeffa857639294aa32666ed
vertesy <abel.vertesy@imba.oeaw.ac.at>
2026-02-02 16:45:04 +0100

    ...

--- R/Stringendo.R
+++ R/Stringendo.R
@@ -194,6 +194,7 @@ ifExistsAndTrue <- function(varname = "pi") {
 #'
 #' @export
 ifExistsElse <- function(varname, alternative = "define an alternative", v = FALSE) {
+  .Deprecated("get0")
   if (!is.character(varname)) varname <- deparse(substitute(varname))
   if (v) message("Checking if ", varname, " exists.")
   if (exists(varname)) get(varname) else alternative
